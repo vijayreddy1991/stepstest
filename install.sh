db28193fcdcad606d49b9cd04162e73c15dd9745
vijayreddy1991 <31643910+vijayreddy1991@users.noreply.github.com>
2020-01-03 11:59:42 +0530

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,23 @@
+#!/bin/bash -e
+
+sudo apt-get update
+
+sudo apt install bash-completion
+
+sudo apt-get install sed
+
+sudo apt-get install coreutils
+
+sudo apt-get install jq
+
+sudo apt-get install gawk
+
+sudo apt install git
+
+sudo apt-get install -y gettext
+
+sudo apt install curl
+
+sudo apt-get install -y cl-base64
+
+sudo apt-get install openssl
