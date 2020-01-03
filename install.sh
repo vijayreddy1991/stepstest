960deb3202e9c3353259571448e03cb763184728
vijayreddy1991 <vijayreddy.chenna@gmail.com>
2020-01-03 08:03:00 +0000

    updated

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
-#!/bin/bash -e
+apt-get update
 
-sudo apt-get update
+apt-get install sudo
 
 sudo apt install bash-completion
 
@@ -8,16 +8,16 @@ sudo apt-get install sed
 
 sudo apt-get install coreutils
 
-sudo apt-get install jq
+sudo apt-get install -y jq
 
-sudo apt-get install gawk
+sudo apt-get install -y gawk
 
-sudo apt install git
+sudo apt install -y git
 
-sudo apt-get install -y gettext
+sudo apt-get install gettext
 
 sudo apt install curl
 
-sudo apt-get install -y cl-base64
+sudo apt-get install cl-base64
 
 sudo apt-get install openssl
